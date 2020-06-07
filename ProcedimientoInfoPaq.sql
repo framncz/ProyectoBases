29e4d98a5934478115299109e4b080bc57b1e0b3
Charlie-aac <63747875+Charlie-aac@users.noreply.github.com>
2020-06-07 18:31:01 -0500

    Add files via upload

--- ProcedimientoInfoPaq.sql
+++ ProcedimientoInfoPaq.sql
@@ -2,16 +2,15 @@ CREATE OR ALTER PROCEDURE SP_InformacionEnvio(@CODIGO INT) AS
 	--DECLARE
 BEGIN
 	IF @CODIGO IN (SELECT Codigo FROM P_Nacional)
-		SELECT N.Codigo, Tipo='Nacional', N.Direccion, N.Peso_gramos AS Peso, C.Nombre AS Conductor, R.Descripcion AS Ruta, CC.Placa AS [Placa del Camion],CC.Fecha AS Fecha FROM P_Nacional N
-		JOIN R_Conductor RC ON RC.R_C_id=N.R_C_id
-		JOIN Conductor C ON RC.RFC=C.RFC
-		JOIN Ruta R ON R.Ruta_id=RC.Ruta_id
-		JOIN Conductor_Camion CC ON CC.id_CC=N.id_CC
+		SELECT N.Codigo, Tipo='Nacional', N.Direccion, N.Peso_gramos AS Peso, C.Nombre AS Conductor, R.Descripcion AS Ruta, RCC.Placa AS [Placa del Camion],RCC.Fecha AS Fecha FROM P_Nacional N
+		JOIN Ruta_Conductor_Camion RCC ON RCC.id_RCC=N.id_RCC
+		JOIN Conductor C ON RCC.RFC=C.RFC
+		JOIN Ruta R ON R.Ruta_id=RCC.Ruta_id
 		WHERE N.Codigo=@CODIGO;
 	ELSE
 		BEGIN
 			IF @CODIGO IN (SELECT Codigo FROM P_Internacional)
-				SELECT I.Codigo, I.LineaAerea, I.Codigo_local FROM P_Internacional I;
+				SELECT I.Codigo, Tipo='Internacional', I.LineaAerea, I.Codigo_local FROM P_Internacional I;
 			ELSE
 				PRINT('El codigo no esta registrada para envio nacional o internacional, favor de ingresar un codigo valido.');
 		END
