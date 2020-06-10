78c663815a5ef40a084594397b14c2b31c982ba5
Charlie-aac <63747875+Charlie-aac@users.noreply.github.com>
2020-06-10 12:16:14 -0500

    Add files via upload

--- CreacionTablas.sql
+++ CreacionTablas.sql
@@ -43,16 +43,25 @@ Nombre			varchar(50),
 constraint PK_CLocal primary key (Codigo)
 );
 GO
-create table P_Nacional(
+create table Paquete(
 Codigo			numeric(5),
-Ciudad_d		varchar(25),
 Direccion		varchar(50),
 Peso_gramos		numeric(5),
 Destinatario	varchar(50),
+Tipo			char(1),
+constraint PK_Paquete primary key (Codigo),
+constraint CK_Tipo check(Tipo in ('N','I'))
+);
+GO
+create table P_Nacional(
+Codigo			numeric(5),
+Ciudad_d		varchar(25),
 id_RCC			numeric(6),
 constraint PK_Nacional primary key (Codigo),
-constraint FK_Nac_CC foreign key (id_RCC)
-references Ruta_Conductor_Camion (id_RCC)
+constraint FK_Nac_RCC foreign key (id_RCC)
+references Ruta_Conductor_Camion (id_RCC),
+CONSTRAINT FK_NAC_PAQ FOREIGN KEY (Codigo)
+references Paquete(Codigo) 
 );
 GO
 create table P_Internacional(
@@ -60,11 +69,10 @@ Codigo			numeric(5),
 LineaAerea		varchar(25),
 F_Entrega		date,
 Codigo_local	numeric(5),
-Direccion		varchar(50),
-Peso_gramos		numeric(5),
-Destinatario	varchar(50),
 constraint PK_Internacional primary key (Codigo),
 constraint FK_Int_Loc foreign key (Codigo_local)
-references C_local (Codigo) on update cascade
+references C_local (Codigo) on update cascade,
+CONSTRAINT FK_INT_PAQ FOREIGN KEY (Codigo)
+references Paquete(Codigo)
 );
 --DROP DATABASE PROYECTO_FINAL;
--- ProcedimientoInfoPaq.sql
+++ ProcedimientoInfoPaq.sql
@@ -2,10 +2,11 @@ CREATE OR ALTER PROCEDURE SP_InformacionEnvio(@CODIGO INT) AS
 	--DECLARE
 BEGIN
 	IF @CODIGO IN (SELECT Codigo FROM P_Nacional)
-		SELECT N.Codigo, Tipo='Nacional', N.Direccion, N.Peso_gramos AS Peso, C.Nombre AS Conductor, R.Descripcion AS Ruta, RCC.Placa AS [Placa del Camion],RCC.Fecha AS Fecha FROM P_Nacional N
+		SELECT N.Codigo, Tipo='Nacional', P.Direccion, P.Peso_gramos AS Peso, C.Nombre AS Conductor, R.Descripcion AS Ruta, RCC.Placa AS [Placa del Camion],RCC.Fecha AS Fecha FROM P_Nacional N
 		JOIN Ruta_Conductor_Camion RCC ON RCC.id_RCC=N.id_RCC
 		JOIN Conductor C ON RCC.RFC=C.RFC
 		JOIN Ruta R ON R.Ruta_id=RCC.Ruta_id
+		JOIN Paquete P ON P.Codigo=N.Codigo
 		WHERE N.Codigo=@CODIGO;
 	ELSE
 		BEGIN
