59dff2d2a08bcef44764695adb00b2568e08a954
HectorMontoya27 <63759843+HectorMontoya27@users.noreply.github.com>
2020-06-10 18:37:32 -0500

    ProyectoBases_1.3
    Se hace ordenamiento de la creacion de tablas, y se cambia el tamaño de los atributos de carga.

--- CreacionTablas.sql
+++ CreacionTablas.sql
@@ -1,78 +1,78 @@
 --CREATE DATABASE PROYECTO_FINAL;
 GO
 USE [PROYECTO_FINAL]
-create table Camion(
-Placa			varchar(6),
-CargaMax		numeric(5),
-CiudadRes		varchar(25),
-constraint PK_Camion primary key (Placa)
+CREATE TABLE Camion(
+    Placa			  varchar(6),
+    CargaMax		numeric(7),
+    CiudadRes		varchar(25),
+    CONSTRAINT PK_Camion PRIMARY KEY (Placa)
 );
 GO
-create table Ruta(
-Ruta_id			numeric(2),
-Descripcion		varchar(50),
-constraint PK_Ruta primary key (Ruta_id),
-constraint CK_Ruta_id check (Ruta_id between 1 and 10)
+CREATE TABLE Ruta(
+    Ruta_id			  numeric(2),
+    Descripcion		varchar(50),
+    CONSTRAINT PK_Ruta PRIMARY KEY (Ruta_id),
+    CONSTRAINT CK_Ruta_id CHECK (Ruta_id BETWEEN 1 AND 10)
 );
 GO
-create table Conductor(
-RFC				varchar(13),
-Nombre			varchar(25),
-Direccion		varchar(50),
-constraint PK_Conductor primary key (RFC)
+CREATE TABLE Conductor(
+    RFC				  varchar(13),
+    Nombre			varchar(25),
+    Direccion		varchar(50),
+    CONSTRAINT PK_Conductor PRIMARY KEY (RFC)
 );
 GO
-create table Ruta_Conductor_Camion(
-id_RCC			numeric(6),
-RFC				varchar(13),
-Placa			varchar(6),
-Fecha			date constraint DF_CC default getdate(),
-Ruta_id			numeric(2),
-constraint PK_CC primary key (id_RCC),
-constraint FK_Con foreign key (RFC)
-references Conductor (RFC) on update cascade,
-constraint FK_Cam foreign key (Placa)
-references Camion(Placa) on update cascade,
-constraint FK_Ruta foreign key (Ruta_id)
-references Ruta (Ruta_id) on update cascade
+CREATE TABLE Ruta_Conductor_Camion(
+    id_RCC			numeric(6),
+    RFC				  varchar(13),
+    Placa			  varchar(6),
+    Fecha			  date CONSTRAINT DF_CC DEFAULT getdate(),
+    Ruta_id			numeric(2),
+    CONSTRAINT PK_CC PRIMARY KEY (id_RCC),
+    CONSTRAINT FK_Con FOREIGN KEY (RFC)
+    REFERENCES Conductor (RFC) ON UPDATE cascade,
+    CONSTRAINT FK_Cam FOREIGN KEY (Placa)
+    REFERENCES Camion(Placa) ON UPDATE cascade,
+    CONSTRAINT FK_Ruta FOREIGN KEY (Ruta_id)
+    REFERENCES Ruta (Ruta_id) ON UPDATE cascade
 );
 GO
-create table C_local(
-Codigo			numeric(5),
-Nombre			varchar(50),
-constraint PK_CLocal primary key (Codigo)
+CREATE TABLE C_local(
+    Codigo			numeric(5),
+    Nombre			varchar(50),
+    CONSTRAINT PK_CLocal PRIMARY KEY (Codigo)
 );
 GO
-create table Paquete(
-Codigo			numeric(5),
-Direccion		varchar(50),
-Peso_gramos		numeric(5),
-Destinatario	varchar(50),
-Tipo			char(1),
-constraint PK_Paquete primary key (Codigo),
-constraint CK_Tipo check(Tipo in ('N','I'))
+CREATE TABLE Paquete(
+    Codigo		  	numeric(5),
+    Direccion		  varchar(50),
+    Peso_gramos		numeric(6),
+    Destinatario	varchar(50),
+    Tipo			    char(1),
+    CONSTRAINT PK_Paquete PRIMARY KEY (Codigo),
+    CONSTRAINT CK_Tipo CHECK(Tipo IN ('N','I'))
 );
 GO
-create table P_Nacional(
-Codigo			numeric(5),
-Ciudad_d		varchar(25),
-id_RCC			numeric(6),
-constraint PK_Nacional primary key (Codigo),
-constraint FK_Nac_RCC foreign key (id_RCC)
-references Ruta_Conductor_Camion (id_RCC),
-CONSTRAINT FK_NAC_PAQ FOREIGN KEY (Codigo)
-references Paquete(Codigo) 
+    CREATE TABLE P_Nacional(
+    Codigo			numeric(5),
+    Ciudad_d		varchar(25),
+    id_RCC			numeric(6),
+    CONSTRAINT PK_Nacional PRIMARY KEY (Codigo),
+    CONSTRAINT FK_Nac_RCC FOREIGN KEY (id_RCC)
+    REFERENCES Ruta_Conductor_Camion (id_RCC),
+    CONSTRAINT FK_NAC_PAQ FOREIGN KEY (Codigo)
+    REFERENCES Paquete(Codigo)
 );
 GO
-create table P_Internacional(
-Codigo			numeric(5),
-LineaAerea		varchar(25),
-F_Entrega		date,
-Codigo_local	numeric(5),
-constraint PK_Internacional primary key (Codigo),
-constraint FK_Int_Loc foreign key (Codigo_local)
-references C_local (Codigo) on update cascade,
-CONSTRAINT FK_INT_PAQ FOREIGN KEY (Codigo)
-references Paquete(Codigo)
+CREATE TABLE P_Internacional(
+    Codigo			  numeric(5),
+    LineaAerea		varchar(25),
+    F_Entrega		  date,
+    Codigo_local	numeric(5),
+    CONSTRAINT PK_Internacional PRIMARY KEY (Codigo),
+    CONSTRAINT FK_Int_Loc FOREIGN KEY (Codigo_local)
+    REFERENCES C_local (Codigo) ON UPDATE cascade,
+    CONSTRAINT FK_INT_PAQ FOREIGN KEY (Codigo)
+    REFERENCES Paquete(Codigo)
 );
 --DROP DATABASE PROYECTO_FINAL;
